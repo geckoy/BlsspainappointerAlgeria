3978c6f8fa4d0ae82a232621686072f94b3315bb
geckoy <younesheissenmann@gmail.com>
2021-08-03 16:28:19 +0100

    fixed user model problem

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Models;
+namespace App\Models; // use App\Models\User;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
