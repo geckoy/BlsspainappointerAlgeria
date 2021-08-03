818df1ad01101409e4f7595d9bb775aee4ad3e81
geckoy <younesheissenmann@gmail.com>
2021-08-03 17:52:27 +0200

    fixed no sandbox problem

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
   var response = await page.goto('http://example.com/', {
     waitUntil: 'load',
