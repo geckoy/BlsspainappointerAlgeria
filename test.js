328bf115a1ce48b00a58cc7b2b71a0b1bebcb943
geckoy <younesheissenmann@gmail.com>
2021-08-03 16:40:57 +0100

    modified test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("worked nodejs");
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  var response = await page.goto('http://example.com/', {
+    waitUntil: 'load',
+  });
+  console.log(response);
+
+  await browser.close();
+})();
