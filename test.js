1eb68be23f27625248ca1185d26bac8ff2c30c3f
geckoy <younesheissenmann@gmail.com>
2021-08-08 09:41:08 +0100

    proxy server test

--- test.js
+++ test.js
@@ -1,219 +1,247 @@
-/**
- * @Dependecies
- */
- var puppeteer = require('puppeteer');
- var fs = require('fs');
- var readline = require('readline');
- var util = require('util');
- var exec = util.promisify(require('child_process').exec);
- var userAgent = require('user-agents');
- 
- // const http = require('http');
- var set_up_config = async function()
- {
-  var promise = new Promise( myResolve => {
-     var config = {};//"app/lib/config.txt"
-     var configs = readline.createInterface({
-         input: fs.createReadStream("app/lib/config.txt")
-     });
-     const pattern_url = /^url=/i;
-     const pattern_CaptchaWebsite_Key = /^CaptchaWebsite_Key=/i;
-     const pattern_action = /^action=/i;
-     const pattern_center = /^center=/i;
-     const pattern_captchaurl = /^captchaurl=/i;
-     const pattern_loginurl = /^loginurl=/i;
-     const pattern_appointurl = /^appointurl=/i;
-     const pattern_action_login = /^action_login=/i;
-     const pattern_host = /^host=/i;
-     const pattern_user = /^user=/i;
-     const pattern_password = /^password=/i;
-     const pattern_database = /^database=/i;
-     
-     configs.on('line', function (line) {
-         if(pattern_url.test(line))
-         {
-             config.url = line.replace(pattern_url, "");
-     
-         }else if(pattern_CaptchaWebsite_Key.test(line))
-         {
-             config.CaptchaWebsite_Key = line.replace(pattern_CaptchaWebsite_Key, "");
-     
-         }else if(pattern_action.test(line))
-         {
-             config.action = line.replace(pattern_action, "");
-     
-         }else if(pattern_center.test(line))
-         {
-             config.center = line.replace(pattern_center, "");
-             
-         }else if(pattern_appointurl.test(line))
-         {
-             config.appointurl = line.replace(pattern_appointurl, "");
- 
-         }else if(pattern_captchaurl.test(line))
-         {
-             config.captchaurl = line.replace(pattern_captchaurl, "");
- 
-         }else if(pattern_loginurl.test(line))
-         {
-             config.loginurl = line.replace(pattern_loginurl, "");
- 
-         }else if(pattern_host.test(line))
-         {
-             config.host = line.replace(pattern_host, "");
- 
-         }else if(pattern_user.test(line))
-         {
-             config.user = line.replace(pattern_user, "");
- 
-         }else if(pattern_password.test(line))
-         {
-             config.password = line.replace(pattern_password, "");
- 
-         }else if(pattern_database.test(line))
-         {
-             config.database = line.replace(pattern_database, "");
- 
-         }else if(pattern_action_login.test(line))
-         {
-             config.action_login = line.replace(pattern_action_login, "");
- 
-         }
-         myResolve(config);
-     });
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless:false,
+    args: [ '--proxy-server=85.208.104.25:45785' ]
   });
+  
+  const page = await browser.newPage();
+  await page.goto('https://icanhazip.com/');
+  //await page.screenshot({ path: 'example.png' });
+  var ip_adress = await page.evaluate(() => {
+    $("body").text();
+  });
+  console.log(ip_adress);
+  //await browser.close();
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * @Dependecies
+//  */
+//  var puppeteer = require('puppeteer');
+//  var fs = require('fs');
+//  var readline = require('readline');
+//  var util = require('util');
+//  var exec = util.promisify(require('child_process').exec);
+//  var userAgent = require('user-agents');
  
-     return promise;
- };
+//  // const http = require('http');
+//  var set_up_config = async function()
+//  {
+//   var promise = new Promise( myResolve => {
+//      var config = {};//"app/lib/config.txt"
+//      var configs = readline.createInterface({
+//          input: fs.createReadStream("app/lib/config.txt")
+//      });
+//      const pattern_url = /^url=/i;
+//      const pattern_CaptchaWebsite_Key = /^CaptchaWebsite_Key=/i;
+//      const pattern_action = /^action=/i;
+//      const pattern_center = /^center=/i;
+//      const pattern_captchaurl = /^captchaurl=/i;
+//      const pattern_loginurl = /^loginurl=/i;
+//      const pattern_appointurl = /^appointurl=/i;
+//      const pattern_action_login = /^action_login=/i;
+//      const pattern_host = /^host=/i;
+//      const pattern_user = /^user=/i;
+//      const pattern_password = /^password=/i;
+//      const pattern_database = /^database=/i;
+     
+//      configs.on('line', function (line) {
+//          if(pattern_url.test(line))
+//          {
+//              config.url = line.replace(pattern_url, "");
+     
+//          }else if(pattern_CaptchaWebsite_Key.test(line))
+//          {
+//              config.CaptchaWebsite_Key = line.replace(pattern_CaptchaWebsite_Key, "");
+     
+//          }else if(pattern_action.test(line))
+//          {
+//              config.action = line.replace(pattern_action, "");
+     
+//          }else if(pattern_center.test(line))
+//          {
+//              config.center = line.replace(pattern_center, "");
+             
+//          }else if(pattern_appointurl.test(line))
+//          {
+//              config.appointurl = line.replace(pattern_appointurl, "");
+ 
+//          }else if(pattern_captchaurl.test(line))
+//          {
+//              config.captchaurl = line.replace(pattern_captchaurl, "");
+ 
+//          }else if(pattern_loginurl.test(line))
+//          {
+//              config.loginurl = line.replace(pattern_loginurl, "");
+ 
+//          }else if(pattern_host.test(line))
+//          {
+//              config.host = line.replace(pattern_host, "");
+ 
+//          }else if(pattern_user.test(line))
+//          {
+//              config.user = line.replace(pattern_user, "");
+ 
+//          }else if(pattern_password.test(line))
+//          {
+//              config.password = line.replace(pattern_password, "");
+ 
+//          }else if(pattern_database.test(line))
+//          {
+//              config.database = line.replace(pattern_database, "");
+ 
+//          }else if(pattern_action_login.test(line))
+//          {
+//              config.action_login = line.replace(pattern_action_login, "");
+ 
+//          }
+//          myResolve(config);
+//      });
+//   });
+ 
+//      return promise;
+//  };
  
  
 
- var get_cli_arg = async function()
- {
-     var arguments =  process.argv.slice(2);
-     var _arg = {};
-     if(arguments.length === 0 )
-     {
-         await page.close();
-         await browser.close();
-         console.log(0);
-         console.log("empty argument applicant_logger");
-         process.exit();
-     }
-    arguments.forEach((currentValue, index, arr) => {
+//  var get_cli_arg = async function()
+//  {
+//      var arguments =  process.argv.slice(2);
+//      var _arg = {};
+//      if(arguments.length === 0 )
+//      {
+//          await page.close();
+//          await browser.close();
+//          console.log(0);
+//          console.log("empty argument applicant_logger");
+//          process.exit();
+//      }
+//     arguments.forEach((currentValue, index, arr) => {
     
-        if(index == 0)
-        {
-            _arg.email = currentValue;
+//         if(index == 0)
+//         {
+//             _arg.email = currentValue;
     
-        }else if(index == 1)
-        {
-            _arg.password = currentValue;
+//         }else if(index == 1)
+//         {
+//             _arg.password = currentValue;
     
-        }else if(index == 2)
-        {
-            _arg.blspass = currentValue;
+//         }else if(index == 2)
+//         {
+//             _arg.blspass = currentValue;
     
-        }else if(index == 3)
-        {
-            _arg.phone = currentValue;
+//         }else if(index == 3)
+//         {
+//             _arg.phone = currentValue;
     
-        }else if(index == 4)
-        {
-            _arg.center = currentValue;
+//         }else if(index == 4)
+//         {
+//             _arg.center = currentValue;
     
-        }else if(index == 5)
-        {
-            _arg.membercount = currentValue;
+//         }else if(index == 5)
+//         {
+//             _arg.membercount = currentValue;
 
-        }else if(index == 6)
-        {
-            if((/%/im).test(currentValue))
-            {
+//         }else if(index == 6)
+//         {
+//             if((/%/im).test(currentValue))
+//             {
                 
-                _arg.entryfor = [];
-                var ppl_array = currentValue.split("%");
-                ppl_array.forEach((currentValue_ppl, index, arr) => {
-                    var ppl_array_data = currentValue_ppl.split(",");
-                    var ppl = {};
-                    ppl_array_data.forEach((currentValue_ppldata, index, arr) => {
-                        if(index == 0)
-                        {
-                            ppl.family_name = currentValue_ppldata
-                        }else if(index == 1)
-                        {
-                            ppl.first_name = currentValue_ppldata
-                        }else if(index == 2)
-                        {
-                            ppl.passport = currentValue_ppldata
-                        }else if(index == 3)
-                        {
-                            ppl.born = currentValue_ppldata
-                        }else if(index == 4)
-                        {
-                            ppl.passportsub = currentValue_ppldata
-                        }else if(index == 5)
-                        {
-                            ppl.passportex = currentValue_ppldata
-                        }else if(index == 6)
-                        {
-                            ppl.passportplace = currentValue_ppldata
-                        }
-                    });
-                    (_arg.entryfor).push(ppl);
-                });
-            }else{
-                var ppl = {};
-                _arg.entryfor = [];
-                var ppl_array_data = currentValue.split(",");
-                ppl_array_data.forEach((currentValue_ppldata, index, arr) => {
-                    if(index == 0)
-                    {
-                        ppl.family_name = currentValue_ppldata
-                    }else if(index == 1)
-                    {
-                        ppl.first_name = currentValue_ppldata
-                    }else if(index == 2)
-                    {
-                        ppl.passport = currentValue_ppldata
-                    }else if(index == 3)
-                    {
-                        ppl.born = currentValue_ppldata
-                    }else if(index == 4)
-                    {
-                        ppl.passportsub = currentValue_ppldata
-                    }else if(index == 5)
-                    {
-                        ppl.passportex = currentValue_ppldata
-                    }else if(index == 6)
-                    {
-                        ppl.passportplace = currentValue_ppldata
-                    }
-                });
-                (_arg.entryfor).push(ppl);
-            }
-        }
-    });
+//                 _arg.entryfor = [];
+//                 var ppl_array = currentValue.split("%");
+//                 ppl_array.forEach((currentValue_ppl, index, arr) => {
+//                     var ppl_array_data = currentValue_ppl.split(",");
+//                     var ppl = {};
+//                     ppl_array_data.forEach((currentValue_ppldata, index, arr) => {
+//                         if(index == 0)
+//                         {
+//                             ppl.family_name = currentValue_ppldata
+//                         }else if(index == 1)
+//                         {
+//                             ppl.first_name = currentValue_ppldata
+//                         }else if(index == 2)
+//                         {
+//                             ppl.passport = currentValue_ppldata
+//                         }else if(index == 3)
+//                         {
+//                             ppl.born = currentValue_ppldata
+//                         }else if(index == 4)
+//                         {
+//                             ppl.passportsub = currentValue_ppldata
+//                         }else if(index == 5)
+//                         {
+//                             ppl.passportex = currentValue_ppldata
+//                         }else if(index == 6)
+//                         {
+//                             ppl.passportplace = currentValue_ppldata
+//                         }
+//                     });
+//                     (_arg.entryfor).push(ppl);
+//                 });
+//             }else{
+//                 var ppl = {};
+//                 _arg.entryfor = [];
+//                 var ppl_array_data = currentValue.split(",");
+//                 ppl_array_data.forEach((currentValue_ppldata, index, arr) => {
+//                     if(index == 0)
+//                     {
+//                         ppl.family_name = currentValue_ppldata
+//                     }else if(index == 1)
+//                     {
+//                         ppl.first_name = currentValue_ppldata
+//                     }else if(index == 2)
+//                     {
+//                         ppl.passport = currentValue_ppldata
+//                     }else if(index == 3)
+//                     {
+//                         ppl.born = currentValue_ppldata
+//                     }else if(index == 4)
+//                     {
+//                         ppl.passportsub = currentValue_ppldata
+//                     }else if(index == 5)
+//                     {
+//                         ppl.passportex = currentValue_ppldata
+//                     }else if(index == 6)
+//                     {
+//                         ppl.passportplace = currentValue_ppldata
+//                     }
+//                 });
+//                 (_arg.entryfor).push(ppl);
+//             }
+//         }
+//     });
 
-   return _arg;
- };
+//    return _arg;
+//  };
 
-var otp_token_command = async function(checkermail, checkerpass) {
-    const { stdout, stderr } = await exec( 'php artisan bls:requestotp '+ checkermail + " " + checkerpass);
-    if(stderr) throw "error";
-    return  stdout.trim();
-};
+// var otp_token_command = async function(checkermail, checkerpass) {
+//     const { stdout, stderr } = await exec( 'php artisan bls:requestotp '+ checkermail + " " + checkerpass);
+//     if(stderr) throw "error";
+//     return  stdout.trim();
+// };
 
-get_cli_arg().then(
-    val => console.log(val)
-)
+// get_cli_arg().then(
+//     val => console.log(val)
+// )
 
 
 
-function sleep(ms) 
-{
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) 
+// {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
 
 
